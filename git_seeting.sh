c9d7b69880a5ce8eec382ab51bfec1e19b72c512
redisread <wujiahong199810222@outlook.com>
2021-08-28 04:23:38 +0000

    change shell

---
+++ git_seeting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function userInfoSet(){
+  git config user.name "redisread"
+  git config user.email "wujiahong199810222@outlook.com"
+}
+
+function main(){
+  userInfoSet
+}
+main
--- system_mirros_seeting.sh
+++ system_mirros_seeting.sh
@@ -3,10 +3,10 @@
 function centosMirros(){
   yum install wget -y
   centos_version=`cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'`
-  mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+  #mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
   if [ $centos_version -eq 6 ]; then
     wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
-  elif [ $centos_versionc -eq 7]; then
+  elif [ $centos_version -eq 7 ]; then
     wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
   fi
 }
@@ -16,7 +16,7 @@ function ubuntuMirros(){
 
 }
 function main(){
-  if [ ! -d "/etc/redhat-release" ]; then
+  if [ ! -f "/etc/redhat-release" ]; then
     ubuntuMirros
   else
     centosMirros
